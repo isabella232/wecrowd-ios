1ae7b5d1987bbec0288c34a75493db7d2250002a
Zach Vega-Perkins <zachv@wepay.com>
2016-09-12 16:07:23 -0700

    added comments explaining functionality

--- WeCrowd/Source/Internal/WCModelProcessor.h
+++ WeCrowd/Source/Internal/WCModelProcessor.h
@@ -3,6 +3,9 @@
 //  WeCrowd
 //
 //  Created by Zach Vega-Perkins on 6/25/15.
+//  This class takes generic data and turns it into wecrowd-specific models. For instance,
+//  a web request might return a JSON object that gets turned into NSBlahDataStructure.
+//  This takes NSBlahDataStructure and outputs a wecrowd model for use in the app.
 //  Copyright (c) 2015 WePay. All rights reserved.
 //
 
@@ -14,6 +17,7 @@ typedef void (^WCModelProcessorCompletion) (id model, NSError *error);
 
 @interface WCModelProcessor : NSObject
 
+// Returns array of campaign models.
 + (NSArray *) createProcessedArrayForCampaigns:(NSArray *) campaigns;
 
 + (void) createCampaignDetailFromDictionary:(NSDictionary *) dictionary

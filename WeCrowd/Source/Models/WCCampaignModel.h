accdb9ad6ba50c846af2b0d2e918444cddad4daa
Zach Vega-Perkins <zachv@wepay.com>
2016-09-15 15:26:03 -0700

    added fresh campaign model to house all campaign data

---
+++ WeCrowd/Source/Models/WCCampaignModel.h
@@ -0,0 +1,13 @@
+//
+//  WCCampaignModel.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 9/15/16.
+//  Copyright © 2016 WePay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCCampaignModel : NSObject
+
+@end

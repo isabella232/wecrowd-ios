69a32b910860973a5a671cb2c5588cef3b1f7cd5
Zach Vega-Perkins <zachv@wepay.com>
2016-09-15 15:42:50 -0700

    added combined campaign member variables to campaign model

--- WeCrowd/Source/Models/WCCampaignModel.h
+++ WeCrowd/Source/Models/WCCampaignModel.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface WCCampaignModel : NSObject
+
+@property (nonatomic, strong) NSString* campaignID;
+@property (nonatomic, strong) NSString* title;
+@property (nonatomic, strong) NSString* detailDescription;
+
+@property (nonatomic) CGFloat donationTargetAmount;
+@property (nonatomic) CGFloat donationAmount;
+
+@property (nonatomic, strong) NSString* thumbnailImageURLString;
+@property (nonatomic, strong) UIImage* thumbnailImage;
+
+@property (nonatomic, strong) NSDate* dateEnd;
 
 @end
